858601c1a6197d118241a62b63ef1a2c42d9532a
aminaminamin93 <battlefieldcompny@gmail.com>
2023-05-23 02:25:01 +0800

    Remove deleted plugin linked

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
